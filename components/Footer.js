ffaf2580b5a5e966df96d93c7216b0cc666ac1d5
kras174 <kras32a@gmail.com>
2020-06-16 15:03:16 +0500

    Start

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="py-5 bg-dark">
+    <div className="container">
+      <p className="m-0 text-center text-white">
+        Copyright &copy; Портфолио на React и NextJS 2020
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
