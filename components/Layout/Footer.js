a7ea32cd03bcd5cdcb7e98fbf00cf7ab058ada89
kras174 <kras32a@gmail.com>
2020-08-12 21:42:28 +0500

    social icons added

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -4,6 +4,19 @@ const Footer = () => (
       <p className="m-0 text-center text-white">
         Copyright 2020 &copy; Портфолио на React (NextJS) и NodeJS 2020
       </p>
+      <p className="m-0 text-center text-white">
+        <small>
+          Icons made by{" "}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            {" "}
+            www.flaticon.com
+          </a>
+        </small>
+      </p>
     </div>
   </footer>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { Container, Row, Col } from 'reactstrap'
-import Typed from 'react-typed'
+import { Container, Row, Col } from "reactstrap";
+import Typed from "react-typed";
 
 const Home = (props) => {
-  const { user = '' } = props.auth
+  const { user = "" } = props.auth;
   const options = {
-    strings: ['React', 'Redux', 'Next JS'],
+    strings: ["React", "Redux", "Next JS"],
     typeSpeed: 40,
-  }
-  const typed = new Typed('.typed', options)
+  };
+  const typed = new Typed(".typed", options);
 
   return (
     <div className="home-page">
@@ -17,8 +17,8 @@ const Home = (props) => {
             <h1>{`Привет ${user && user.name}`}</h1>
             {user && (
               <small>
-                Я ещё не придумал какой секретный контент показать для тебя, но
-                обязательно подумаю над этим.
+                Я ещё не придумал какой секретный контент показать для тебя, но обязательно подумаю
+                над этим.
               </small>
             )}
           </div>
@@ -28,35 +28,47 @@ const Home = (props) => {
           </div>
           <div className="text-container col-sm-12 col-md-7 mb-5">
             <p>
-              Меня зовут <strong>Антон Красильников</strong>, и я
-              ВЕБ-разработчик.
+              Меня зовут <strong>Антон Красильников</strong>, и я ВЕБ-разработчик.
             </p>
             <p>
-              Прекрасно знаю что такое HTML, CSS, JavaScript, JQuery, и какую
-              красоту с помощью этого всего можно сделать =)
+              Прекрасно знаю что такое HTML, CSS, JavaScript, JQuery, и какую красоту с помощью
+              этого всего можно сделать =)
             </p>
             <p>
-              Специализируюсь на разработке ВЕБ-приложений с использованием
-              таких фреймворков как{' '}
+              Специализируюсь на разработке ВЕБ-приложений с использованием таких фреймворков как{" "}
               <strong>
                 <Typed
-                  strings={['React.', 'Redux.', 'Next JS.']}
+                  strings={["React.", "Redux.", "Next JS."]}
                   typeSpeed={40}
                   backSpeed={50}
                   loop
                 />
               </strong>
             </p>
-            <strong>
-              <a href="https://github.com/kras174" target="_blank">
-                Мой GIT реппозиторий
+            <div className="social-container">
+              <p>Будем на связи:</p>
+              <a href="https://vk.com/id238622" target="_blank">
+                <img src="/images/socials/vk.png" alt="vk" />
               </a>
-            </strong>
+              <a href="https://t.me/kras174" target="_blank">
+                <img src="/images/socials/telegram.png" alt="telegram" />
+              </a>
+              <a href="https://wa.me/79823216648" target="_blank">
+                <img src="/images/socials/whatsapp.png" alt="whatsapp" />
+              </a>
+              <a href="Skype:kras85?chat" target="_blank">
+                <img src="/images/socials/skype.png" alt="skype" />
+              </a>
+              <a href="https://github.com/kras174" target="_blank">
+                <img src="/images/socials/github.png" alt="github" />
+              </a>
+            </div>
           </div>
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+8;
